0b850b470bd3e73e1d4c9be31dcb81385c671786
I-Vishal-kumar <vishal2983kumar@gmail.com>
2024-12-15 10:29:45 +0530

    update

---
+++ novellfootball/src/app/api/gateway/route.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import md5 from "md5";
+import { NextResponse } from "next/server";
+
+
+export async function POST (request,res){
+
+    const {amount} = await request.json();
+    
+    const merchantKey = "6c567b06cd558af505bbee0271612be0";
+        const merchantId = "mer714043"; 
+        const orderId = `${Date.now()}`;
+  
+        // Data to be signed
+        const signStr = `firstName=john&lastName=tom&merchantNo=${merchantId}&orderAmt=${orderAmt}&orderNo=${orderId}&productCode=80003`;
+        const sign = md5(`${signStr}&key=${merchantKey}`);
+  
+        const requestBody = {
+          "merchantNo": merchantId,
+          "orderNo": orderId,
+          "orderAmt": `${amount}`,
+          "productCode": "80003",
+          "notifyUrl": "https://parlourfootball.online/api/callback",
+          "accCode": "INR_BANK_A",
+          "otherPara2": "WALLET",
+          "otherPara1": "GCASH",
+          "firstName": "john",
+          "lastName": "tom",
+          "sign": sign,
+        };
+  
+        try {
+            const response = await axios.post('https://api.carry-pay.com/api/agentPay/payOrder', requestBody);
+            console.warn(response);
+            const result = response.data;
+
+            return NextResponse.json({ result })
+            // if (result.isValid && result.url) {
+            //     // Redirect to the payment URL
+            // } else {
+            //     console.error("Payment initiation failed:", result.message);
+            //     alert("Payment initiation failed. Check console for details.");
+            // }
+        } catch (error) {
+            return NextResponse.json({err: error.message})
+            console.error("Error making payment request:", error);
+            alert("Error occurred while making payment.");
+        }
+}
--- novellfootball/src/app/gateway/page.js
+++ novellfootball/src/app/gateway/page.js
@@ -1,57 +1,18 @@
 "use client"
+import axios from "axios";
 import md5 from "md5";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Page(){
 
-  const router = useRouter();
+  const [amt, setAmt] = useState(100);  
 
   const handlePayment = async (amount) => {
-      const merchantKey = "6c567b06cd558af505bbee0271612be0";
-      const merchantId = "mer714043"; 
-      const orderId = `${Date.now()}`;
+    let res = await axios.post("/api/gateway", {amount})
+    console.log(res);
+  }
 
-      // Data to be signed
-      const signStr = `firstName=john&lastName=tom&merchantNo=${merchantId}&orderAmt=100&orderNo=${orderId}&productCode=80003`;
-      const sign = md5(`${signStr}&key=${merchantKey}`);
-
-      const requestBody = {
-        "merchantNo": merchantId,
-        "orderNo": orderId,
-        "orderAmt": `${amount}`,
-        "productCode": "80003",
-        "notifyUrl": "https://parlourfootball.online/api/callback",
-        "accCode": "INR_BANK_A",
-        "otherPara2": "WALLET",
-        "otherPara1": "GCASH",
-        "firstName": "john",
-        "lastName": "tom",
-        "sign": sign,
-      };
-
-      try {
-          const response = await fetch("https://api.carry-pay.com/api/agentPay/payOrder", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(requestBody),
-          });
-
-          const result = await response.json();
-          console.warn(result);
-          if (result.isValid && result.url) {
-              // Redirect to the payment URL
-              router.push(result.url);
-          } else {
-              console.error("Payment initiation failed:", result.message);
-              alert("Payment initiation failed. Check console for details.");
-          }
-      } catch (error) {
-          console.error("Error making payment request:", error);
-          alert("Error occurred while making payment.");
-      }
-  };
-  const [amt, setAmt] = useState(100);  
   return (
       <div>
           <h1>CarryPay Test Payment</h1>
