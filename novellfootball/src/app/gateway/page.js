a30bd3b48c9d323ddd274e923c1bcd60749398f1
Suraj90127 <114793655+Suraj90127@users.noreply.github.com>
2024-12-16 23:00:42 +0530

    rj

--- novellfootball/src/app/gateway/page.js
+++ novellfootball/src/app/gateway/page.js
@@ -1,23 +1,27 @@
-"use client"
+"use client";
 import axios from "axios";
 import { useState } from "react";
 
-export default function Page(){
-
-  const [amt, setAmt] = useState(100);  
-  const [productCode, setproductCode] = useState(80003);  
+export default function Page() {
+  const [amt, setAmt] = useState(100);
+  const [productCode, setproductCode] = useState("90003");
 
   const handlePayment = async (amount) => {
-    let res = await axios.post("/api/gateway", {amount, productCode})
+    let res = await axios.post("/api/gateway", { amount, productCode });
     console.log(res);
-  }
+  };
 
   return (
-      <div>
-          <h1>CarryPay Test Payment</h1>
-            <input value={amt} onChange={(e)=>setAmt(e.target.value)} />
-            <input value={productCode} onChange={(e)=>setproductCode(e.target.value)} />
-            <button className="bg-blue-300" onClick={() => handlePayment(amt)}>Pay {amt}</button>
-      </div>
+    <div>
+      <h1>CarryPay Test Payment</h1>
+      <input value={amt} onChange={(e) => setAmt(e.target.value)} />
+      <input
+        value={productCode}
+        onChange={(e) => setproductCode(e.target.value)}
+      />
+      <button className="bg-blue-300" onClick={() => handlePayment(amt)}>
+        Pay {amt}
+      </button>
+    </div>
   );
 }
